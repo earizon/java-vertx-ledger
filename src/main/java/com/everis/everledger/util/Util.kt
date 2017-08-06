c349627d704a0165b12f4aad493f789159af4788
Enrique Arizón Benito <enrique.arizon.benito@everis.com>
2017-08-06 09:00:11 +0000

    Refactoring java to kotlin

--- src/main/java/com/everis/everledger/util/Util.kt
+++ src/main/java/com/everis/everledger/util/Util.kt
@@ -1,8 +1,12 @@
 package com.everis.everledger.util
 
+import com.everis.everledger.Config
+import com.everis.everledger.HTTPInterledgerException
 import org.apache.commons.lang3.math.NumberUtils
 import org.interledger.Condition
 import org.interledger.Fulfillment
+import org.interledger.InterledgerAddress
+import org.interledger.ilp.InterledgerError
 import java.lang.Long
 import java.net.URI
 import java.time.ZoneId
@@ -19,10 +23,7 @@ object TimeUtils {
 
 object ConversionUtil {
 
-    fun toNumber(value: Any): Number {
-        if (value is Number) { return value }
-        return NumberUtils.createNumber(value.toString())
-    }
+    fun toNumber(value: Any): Number = if (value is Number) value else NumberUtils.createNumber(value.toString())
 
     fun fulfillmentToBase64(FF: Fulfillment): String {
         var response = Base64.getEncoder().encodeToString(FF.preimage)
@@ -32,7 +33,7 @@ object ConversionUtil {
 
     /**
      * Parses a URI formatted crypto-condition
-
+     *
      * @param uri
      * *  The crypto-condition formatted as a uri.
      * *
@@ -58,5 +59,44 @@ object ConversionUtil {
         val fingerprint = Base64.getUrlDecoder().decode(m.group(2))
         return Condition.builder().hash(fingerprint).build()
     }
-
 }
+
+object ILPExceptionSupport {
+    private val selfAddress = InterledgerAddress.builder().value(Config.ilpPrefix).build()
+    /**
+     * Well known ILP Errors as defined in the RFCs
+     * @param errCode
+     *
+     * @param data
+     */
+    fun createILPException(httpErrCode: Int, errCode: InterledgerError.ErrorCode, data: String): HTTPInterledgerException =
+        HTTPInterledgerException(httpErrCode, InterledgerError(errCode, /* triggeredBy */ selfAddress,
+                ZonedDateTime.now(), ArrayList<InterledgerAddress>(), /*self Address*/ selfAddress, data))
+
+    // Next follow some wrappers arount createILPException, more human-readable.
+    // ----------- Internal --------------
+    fun createILPInternalException(data: String): HTTPInterledgerException =
+         createILPException(500, InterledgerError.ErrorCode.T00_INTERNAL_ERROR, data)
+
+    // ------------ Unauthorized ------------- // TODO:(RFC) Use new ErrorCode.??_UNAUTHORIZED
+    @JvmOverloads fun createILPUnauthorizedException(data: String = "Unauthorized"): HTTPInterledgerException =
+        createILPException(401, InterledgerError.ErrorCode.T00_INTERNAL_ERROR, data)
+
+    // ----------- Forbidden --------------// TODO:(RFC) Use new ErrorCode.??_FORBIDDEN
+    @JvmOverloads fun createILPForbiddenException(data: String = "Forbidden"): HTTPInterledgerException =
+        createILPException(403, InterledgerError.ErrorCode.T00_INTERNAL_ERROR, "data")
+
+    // ------------ NotFound ------------- // TODO:(ILP) Use new ErrorCode.??_NOT_FOUND
+    @JvmOverloads fun createILPNotFoundException(data: String = "Not Found"): HTTPInterledgerException =
+        createILPException(404, InterledgerError.ErrorCode.T00_INTERNAL_ERROR, data)
+
+    // ------------- BadRequest ------------
+    @JvmOverloads fun createILPBadRequestException(data: String = "Forbidden"): HTTPInterledgerException =
+        createILPException(400, InterledgerError.ErrorCode.F00_BAD_REQUEST, data)
+
+    // ------------- Unprocessable Entity ------------
+    @JvmOverloads fun createILPUnprocessableEntityException(data: String = "Unprocessable"): HTTPInterledgerException =
+        createILPException(422, InterledgerError.ErrorCode.F00_BAD_REQUEST, data)
+}
+
+
