6ccb30ba82a6b9344a0b3e50a9c11ff781704065
Enrique Arizón Benito <enrique.arizon.benito@everis.com>
2017-08-28 12:02:23 +0000

    Add TODOs

--- src/main/java/com/everis/everledger/ifaces/transfer/IfaceILPSpecTransferManager.java
+++ src/main/java/com/everis/everledger/ifaces/transfer/IfaceILPSpecTransferManager.java
@@ -6,10 +6,11 @@ import org.interledger.Condition;
 import org.interledger.Fulfillment;
 
 public interface IfaceILPSpecTransferManager {
-
+   // TODO:(0)
     java.util.List<IfaceTransfer> getTransfersByExecutionCondition(Condition condition);
 
-    void createNewRemoteILPTransfer(IfaceTransfer newTransfer);
+    // TODO:(0) proposeILPTransfer(ILPTransfer
+    void prepareILPTransfer(IfaceTransfer newTransfer); // TODO:(0) Rename / split into propose/prepare ?
 
     IfaceTransfer executeILPTransfer(IfaceTransfer transfer, Fulfillment executionFulfillment);
 
