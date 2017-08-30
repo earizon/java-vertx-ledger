d354732bc5eca2cd07417a9f79afd31de7b94976
Enrique Arizón Benito <enrique.arizon.benito@everis.com>
2017-08-30 07:29:23 +0000

    Add Ethereum Iface to IfaceTransferManager

--- src/main/java/com/everis/everledger/ifaces/transfer/IfaceTransferManager.java
+++ src/main/java/com/everis/everledger/ifaces/transfer/IfaceTransferManager.java
@@ -1,3 +1,7 @@
 package com.everis.everledger.ifaces.transfer;
 
-public interface IfaceTransferManager extends IfaceLocalTransferManager, IfaceILPSpecTransferManager {}
+public interface IfaceTransferManager extends
+        IfaceLocalTransferManager,
+        IfaceILPSpecTransferManager,
+        IfaceEthereumBlockchain
+        {}
